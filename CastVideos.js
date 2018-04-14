9077f512c85d9255c9d61e6f243a05ccc63cd394
Adam Wootton <ajwootto@uwaterloo.ca>
2018-04-14 17:49:24 -0400

    remove gpm api

--- CastVideos.js
+++ CastVideos.js
@@ -933,7 +933,7 @@ var mediaJSON = { 'categories' : [{ 'name' : 'Movies',
         { 'description' : "Testing playback of GPM<",
             'sources' : ['http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
             'subtitle' : 'GPM playing',
-            'thumb' : 'images/BigBuckBunny.jpg',
+            'thumb' : 'images/gpm.jpg',
             'title' : 'Test GPM Playback'
         },
         { 'description' : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
